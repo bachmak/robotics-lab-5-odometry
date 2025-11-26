93f602e83d643ca6e70833343f39e365f933197b
Dima Kochetov <dima.kochetov.70@gmail.com>
2025-11-26 22:41:50 +0100

    Pass speed to robot_utils

--- include/robot_utils.h
+++ include/robot_utils.h
@@ -1,29 +1,30 @@
 #pragma once
 
+#include "robot.h"
 #include "common.h"
 #include "geo_utils.h"
 
 namespace robot_utils
 {
-    inline void move_fwd(Robot &robot, Meter distance)
+    inline void move_fwd(Robot &robot, Meter distance, Speed speed)
     {
-        robot.rotate(distance, distance, Speed{1});
+        robot.rotate(distance, distance, speed);
     }
 
-    inline void move_bwd(Robot &robot, Meter distance)
+    inline void move_bwd(Robot &robot, Meter distance, Speed speed)
     {
-        robot.rotate(-distance, -distance, Speed{1});
+        robot.rotate(-distance, -distance, speed);
     }
 
-    inline void rotate_left(Robot &robot, Degree angle)
+    inline void rotate_left(Robot &robot, Degree angle, Speed speed)
     {
         auto sector_length = geo_utils::to_sector(angle, robot.width() / 2);
-        robot.rotate(-sector_length, sector_length, Speed{1});
+        robot.rotate(-sector_length, sector_length, speed);
     }
 
-    inline void rotate_right(Robot &robot, Degree angle)
+    inline void rotate_right(Robot &robot, Degree angle, Speed speed)
     {
         auto sector_length = geo_utils::to_sector(angle, robot.width() / 2);
-        robot.rotate(sector_length, -sector_length, Speed{1});
+        robot.rotate(sector_length, -sector_length, speed);
     }
 }
